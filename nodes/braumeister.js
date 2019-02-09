e06ec29cbe45f8bc01461af9a407765febbb10c9
Thomas Sauter <sauter@sauter-held.de>
2019-02-09 13:50:00 +0100

    add new properties to payload

--- nodes/braumeister.js
+++ nodes/braumeister.js
@@ -20,14 +20,36 @@ module.exports = function(RED) {
           const body = response.body.split(";");
           const data = body[2].split("X");
 
+          let interrupt = Number(data[15][0]);
+          let editmode = Number(data[15][2])
+          let beep = Number(data[15][1]);
+          let cover = 0;
+          if (data[15][1] === 'D') {
+            interrupt = 1;
+            cover = 1;
+            beep = 1;
+          }
+
           const payload = {
             firmware_version: body[0],
-            temperature_target: Number(data[4]),
+            language: Number(data[0]),
+            local_time: data[1],
+            status: Number(data[2]),
+            temperature_target: Number(data[4]) / 10,
             temperature_current: Number(data[5]),
             time_target: Number(data[6]),
             time_elapsed: Number(data[7]),
+            recipe: Number(data[9]),
+            rast: Number(data[10]),
+            hop: Number(data[11]),
+            progress: Number(data[12]) / 318 * 100,
+            buttons: data[13],
             pump: { p: "off", P: "on", q: "inactive" }[data[14][0]],
             heating: { h: "off", H: "on", i: "inactive" }[data[14][1]],
+            interrupt: interrupt,
+            cover: cover,
+            beep: beep,
+            nexthop: Number(data[18]),
             source_string: response.body
           };
 
